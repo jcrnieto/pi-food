6303ab351850a258fdd49522814e5b4350b1be01
jcrnieto <82995532+jcrnieto@users.noreply.github.com>
2022-06-24 01:10:59 -0300

    'correcciones'

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import {Link} from 'react-router-dom';
-
+import './landingPage.css'
 export default function LandingPage(){
     return(
         <div className='landing'>
             <h1 className="titulos bienvenido">Bienvenidos a mi página</h1>
             <Link to={'/home'}>
-                <button className="botones-simples">Ingresar</button>
+                <button className="boton-landing">Ingresar</button>
             </Link>
         </div>
     )
